a5f8d2379e13cf4b635ee5fa44fd1bf4a61d8b04
Anthony Re <re@towson.edu>
2018-05-17 09:26:47 -0400

    updated scaffolding to remove deprecated modules, automated testing

--- webpack.config.js
+++ webpack.config.js
@@ -2,91 +2,97 @@
 const webpack = require( 'webpack' );
 
 module.exports = {
-    entry: [
-        'babel-polyfill',
-        './Client/Scripts/index.jsx'
-    ],
+    entry: {
+        main: [
+            'babel-polyfill',
+            path.resolve( __dirname, 'client/index/index.jsx' )
+        ]
+    },
     output: {
         path: path.resolve( __dirname, './' ),
         filename: '[name].bundle.js',
         sourceMapFilename: '[name].bundle.map'
     },
+    stats: "minimal",
     module: {
         rules: [
-          {
-              test: /\.css$/,
-              include: /node_modules/,
-              use: [
-                {
-                    loader: 'style-loader?sourceMap'
-                },
-                {
-                    loader: 'css-loader?importLoaders=1'
-                }
-              ]
-          },
-          {
-              test: /\.css$/,
-              include: path.resolve( __dirname, 'vendor' ),
-              use: [
-                  {
-                      loader: 'style-loader?source-map'
-                  },
-                  {
-                      loader: 'css-loader?-url&importLoaders=1'
-                  }
-              ]
-          },
-          {
-              test: /\.css$/,
-              include: path.resolve( __dirname, 'styles' ),
-              loaders: [
-                {
-                    loader: 'style-loader?sourceMap'
-                },
-                {
-                    loader: 'css-loader?-url&importLoaders=1'
-                }
-              ]
-          },
-          {
-              test: /\.css$/,
-              exclude: [
-                path.resolve( __dirname, 'styles' ),
-                path.resolve( __dirname, 'node_modules' )
-              ],
-              loaders: [
-                {
-                    loader: 'style-loader?sourceMap'
-                },
-                {
-                    loader: 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-                }
-              ]
-          },
-          {
-              test: /\.woff[2]?(\?v=\d+\.\d+\.\d+)?$/,
-              loader: "url-loader?limit=10000&minetype=application/font-woff"
-          },
-          {
-              test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-              loader: "url-loader?limit=10000&minetype=application/octet-stream"
-          },
-          {
-              test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-              loader: "file-loader?context=./app/static"
-          },
-          {
-              test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-              loader: "url-loader?limit=10000&minetype=image/svg+xml"
-          },
-          {
-              test: /\.jsx?$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: 'babel-loader'
-              }
-          }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                include: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader?sourceMap'
+                    },
+                    {
+                        loader: 'css-loader?importLoaders=1'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                include: path.resolve( __dirname, 'vendor' ),
+                use: [
+                    {
+                        loader: 'style-loader?source-map'
+                    },
+                    {
+                        loader: 'css-loader?-url&importLoaders=1'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                include: path.resolve( __dirname, 'styles' ),
+                loaders: [
+                    {
+                        loader: 'style-loader?sourceMap'
+                    },
+                    {
+                        loader: 'css-loader?-url&importLoaders=1'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: [
+                    path.resolve( __dirname, 'styles' ),
+                    path.resolve( __dirname, 'node_modules' )
+                ],
+                loaders: [
+                    {
+                        loader: 'style-loader?sourceMap'
+                    },
+                    {
+                        loader: 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+                    }
+                ]
+            },
+            {
+                test: /\.woff[2]?(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&minetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file-loader?context=./app/static"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&minetype=image/svg+xml"
+            }
         ]
     },
     resolve: {
@@ -100,9 +106,9 @@ module.exports = {
         }
     },
     plugins: [
-      new webpack.ProvidePlugin( {
-          React: "react"
-      } ),
-      new webpack.NamedModulesPlugin()
+        new webpack.ProvidePlugin( {
+            React: "react"
+        } ),
+        new webpack.NamedModulesPlugin()
     ]
 };
